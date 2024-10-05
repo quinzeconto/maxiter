2a82c08ed8c22e2db31227569a762dc11c047644
Victor <mxd46934@gmail.com>
2024-10-05 16:12:42 -0300

    Initial Commit Updates

--- app/models/LogModel.php
+++ app/models/LogModel.php
@@ -13,7 +13,7 @@ class LogModel {
 
         self::$log = mb_strtoupper($log);
 
-        $query = "INSERT INTO logs (log, date_created) VALUES (:log, :date)";
+        $query = "INSERT INTO logs (log, created_at) VALUES (:log, :date)";
         $result = DatabaseModel::connection("maxiter")->execute($query, [
             ":log" => self::$log,
             ":date" => $formattedDateTime,
